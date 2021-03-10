425a0ee41b25b0f1b195e40e82bada920833ee38
OsmanFarukO <osmanfarukorucoglu@gmail.com>
2021-03-11 00:30:45 +0300

    Initial commit

---
+++ app/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+"fmt"
+"net/http"
+)
+
+func main() {
+
+
+http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    name := r.URL.Path[len("/"):]
+    fmt.Fprintf(w, "Hello Turkai from %s\n", name)
+})
+
+http.ListenAndServe(":11130", nil)
+}
