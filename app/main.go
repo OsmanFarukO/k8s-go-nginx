bbe2601bb37be19f0305ca3fd79cd52f5bc24c76
OsmanFarukO <osmanfarukorucoglu@gmail.com>
2021-04-04 13:55:35 +0300

    Update main.go

--- app/main.go
+++ app/main.go
@@ -10,7 +10,7 @@ func main() {
 
 http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
     name := r.URL.Path[len("/"):]
-    fmt.Fprintf(w, "Hello Turkai from %s\n", name)
+    fmt.Fprintf(w, "Hello from %s\n", name)
 })
 
 http.ListenAndServe(":11130", nil)
